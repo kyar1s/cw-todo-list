7b1cdf66011e2f365bd3a815e3004bc7eb636160
Kyaris <iris.ledesma.27@gmail.com>
2022-10-25 15:57:17 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex flex-col h-[100vh] bg-slate-800">
+      <div className="flex items-center justify-center max-w-[1000px]"></div>
+    </div>
+  );
+};
+
+export default App;
