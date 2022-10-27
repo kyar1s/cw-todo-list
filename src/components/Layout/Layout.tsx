eeda411e2674a925cca4b5aa3668b6315d3b3220
Kyaris <iris.ledesma.27@gmail.com>
2022-10-27 22:34:43 +0200

    add clientAddr condition

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,20 +7,24 @@ const Layout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
     <div className="flex flex-col h-[100vh] bg-slate-800">
       <nav className="max-w-[1000px] p-4 mx-auto w-full flex items-center justify-between gap-4">
-        {clientAddr ? (
+        {clientAddr && (
           <>
             <p className="font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-amber-400 via-pink-400 to-indigo-500">
               TODO's
             </p>
             <p className="text-white text-md">{clientAddr}</p>
           </>
-        ) : (
-          <GradientButton onClick={connectWallet}>Log in!</GradientButton>
         )}
       </nav>
-      <div className="flex items-center justify-center max-w-[1000px] w-full mx-auto p-4">
-        {children}
-      </div>
+      {clientAddr ? (
+        <div className="flex items-center justify-center max-w-[1000px] w-full mx-auto p-4 h-full ">
+          {children}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center">
+          <GradientButton onClick={connectWallet}>Log in!</GradientButton>
+        </div>
+      )}
     </div>
   );
 };
