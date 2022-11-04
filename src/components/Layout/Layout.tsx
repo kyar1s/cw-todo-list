4eed5ea1a0097a7c7a0a814a45d4b884790cfbc4
Kyaris <iris.ledesma.27@gmail.com>
2022-11-04 09:57:13 +0100

    add dropdown and fix styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,18 +3,32 @@ import { useAppContext } from "../../providers/AppProvider";
 import { IntlAddress } from "../../utils/intl";
 import { GradientButton } from "../Buttons";
 import { FiGithub } from "react-icons/fi";
+import SimpleDropdown from "../Dropdowns/SimpleDropdown";
 
 const Layout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
-  const { connectWallet, clientAddr } = useAppContext();
+  const { connectWallet, clientAddr, changeChain, chain } = useAppContext();
   return (
-    <div className="flex flex-col bg-slate-800 min-h-screen justify-between">
+    <div className="flex flex-col min-h-screen justify-between">
       <nav className="max-w-[1000px] p-4 mx-auto w-full flex items-center justify-between gap-4">
         {clientAddr && (
           <>
             <p className="font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-amber-400 via-pink-400 to-indigo-500">
               TODO's
             </p>
-            <p className="text-white text-md">{IntlAddress(clientAddr)}</p>
+            <div className="text-white text-md flex gap-4 justify-center items-center">
+              {IntlAddress(clientAddr)}
+              <SimpleDropdown
+                options={[
+                  { name: "Juno Testnet", click: () => changeChain("uni-5") },
+                  {
+                    name: "Malaga 420",
+                    click: () => changeChain("malaga-420"),
+                  },
+                ]}
+              >
+                {chain === "malaga-420" ? "Malaga 420" : "Juno Testnet"}
+              </SimpleDropdown>
+            </div>
           </>
         )}
       </nav>
